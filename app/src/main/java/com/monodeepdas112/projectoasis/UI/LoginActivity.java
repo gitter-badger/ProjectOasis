89d3886df205954ca83293262fffa320ed14ae35
monodeepdas1215 <monodeepdas1215@gmail.com>
2017-03-06 01:34:56 +0530

    Login Page structure and workflow implementation complete

--- app/src/main/java/com/monodeepdas112/projectoasis/UI/LoginActivity.java
+++ app/src/main/java/com/monodeepdas112/projectoasis/UI/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.monodeepdas112.projectoasis.UI;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputLayout;
@@ -7,12 +8,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.monodeepdas112.projectoasis.Interfaces.MyCommonUISetUp;
 import com.monodeepdas112.projectoasis.R;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by monodeep on 5/3/17.
@@ -69,17 +74,25 @@ public class LoginActivity extends AppCompatActivity implements MyCommonUISetUp,
     }
 
     private boolean validateEmail(String email) {
-        //TODO add the code to validate email
-        return true;
+        final String EMAIL_PATTERN = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        Matcher matcher;
+        matcher = pattern.matcher(email);
+        return matcher.matches();
     }
 
     private boolean validatePassword(String password) {
-        //TODO add the code to validate password
-        return true;
+
+        int passwordMaxLength=Integer.parseInt(getString(R.string.password_max_length));
+        int passwordMinLength=Integer.parseInt(getString(R.string.password_min_length));
+        if (password.length()<=passwordMaxLength&&password.length()>=passwordMinLength)
+            return true;
+        else return false;
     }
 
     @Override
     public void onClick(View v) {
+        hideKeyboard();
         switch (v.getId()){
             case R.id.loginButton:
                 emailWrapper.setErrorEnabled(false);
@@ -98,25 +111,31 @@ public class LoginActivity extends AppCompatActivity implements MyCommonUISetUp,
                         break;
                     case EMAIL_EMPTY:
                         emailWrapper.setError(getString(R.string.activity_login_email_error));
+                        emailWrapper.requestFocus();
                         break;
                     case EMAIL_INVALID:
                         emailWrapper.setError(getString(R.string.activity_login_email_error));
+                        emailWrapper.requestFocus();
                         break;
                     case PASSWORD_INVALID:
                         passwordWrapper.setError(getString(R.string.activity_login_password_error));
+                        passwordWrapper.requestFocus();
+                        configureSnackbar(v,WRONG_CREDENTIALS,"Password must be between 8 - 12 charaters long");
                 }
                 break;
             case R.id.forgotButton:
                 Toast.makeText(getApplicationContext(),"Forgot button working properly",
                         Toast.LENGTH_SHORT).show();
                 break;
-            default:
-                hideKeyboard();
         }
     }
 
     private void hideKeyboard() {
-
+        View view = getCurrentFocus();
+        if (view != null) {
+            ((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE)).
+                    hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+        }
     }
 
     private void configureSnackbar(View v,int state,String message){
